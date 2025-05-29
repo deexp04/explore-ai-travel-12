c45d353b2962b532c42a7ff701b9f45bbdaa8bc3
Deebhika Kumaran <106971773+deexp04@users.noreply.github.com>
2025-05-29 12:02:53 +0530

    Update FloatingChatButton.tsx

--- src/components/FloatingChatButton.tsx
+++ src/components/FloatingChatButton.tsx
@@ -35,14 +35,14 @@ const FloatingChatButton = () => {
                 <MessageSquare className="w-6 h-6 text-blue-600" />
                 <span>SyncAgents</span>
               </div> */}
-              <Button 
+{/*               <Button 
                 variant="ghost" 
                 size="sm"
                 onClick={() => setIsOpen(false)}
                 className="text-gray-500 hover:text-gray-700"
               >
-{/*                 <X className="w-5 h-5" /> */}
-              </Button>
+                 <X className="w-5 h-5" /> 
+              </Button>  */}
             </DialogTitle>
           </DialogHeader>
           <div className="px-6 pb-6 h-full">
