5bf95f11371293d6bed2310759517a50c16d09a1
Deebhika Kumaran <106971773+deexp04@users.noreply.github.com>
2025-05-29 12:00:06 +0530

    Update FloatingChatButton.tsx

--- src/components/FloatingChatButton.tsx
+++ src/components/FloatingChatButton.tsx
@@ -41,7 +41,7 @@ const FloatingChatButton = () => {
                 onClick={() => setIsOpen(false)}
                 className="text-gray-500 hover:text-gray-700"
               >
-                <X className="w-5 h-5" />
+{/*                 <X className="w-5 h-5" /> */}
               </Button>
             </DialogTitle>
           </DialogHeader>
